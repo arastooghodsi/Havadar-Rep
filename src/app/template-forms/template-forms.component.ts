192a0b828663a19e5b604e707095e817473b2775
arastooghodsi <arastoo.ghodsi.ai@gmail.com>
2020-09-16 11:30:10 +0430

    16 Sep Mmorning Office

--- src/app/template-forms/template-forms.component.ts
+++ src/app/template-forms/template-forms.component.ts
@@ -8,19 +8,25 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateFormsComponent {
 
+  public formValid = false;
   @ViewChild('f') inputForm: NgForm
-
-  // onSubmit(form: HTMLFormElement) {
-  //   console.log(form);
-  // }
+  @ViewChild('group1') inputValidForm: NgForm
+  // @ViewChild('username') inputUpdateUsername: NgForm
 
   defaultGender = 'male';
   color = '';
   data: any[];
   onSubmit() {
     console.log(this.inputForm);
-    // alert(this.username + " , " + this.email + " , " + this.gender);
-
+    if(!this.inputValidForm.valid) {
+      this.formValid = true;
+    }
+    if(!this.inputValidForm.form.updateOn) {
+      // this.formValid = true;
+      alert('yes');
+    }
+    // if(this.inputUpdateUsername.errors)
+    //   alert('yes');
   }
 
 }
